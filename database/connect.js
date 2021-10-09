355fbe191dff7a3e9e33101e59fe18de8f73bc70
jayteedubya <jayteedubya@gmail.com>
2021-10-08 19:57:52 -0700

    remove dev dependencies

--- database/connect.js
+++ database/connect.js
@@ -3,10 +3,10 @@ const init = {
     noWarnings: false
 }
 const pg = require('pg-promise')(init);
-const monitor = require('pg-monitor');
+//const monitor = require('pg-monitor');
 const credentials = {connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false }};
 const db = pg(credentials);
-monitor.attach(init);
+//monitor.attach(init);
 
 
 module.exports = { pg, db };
